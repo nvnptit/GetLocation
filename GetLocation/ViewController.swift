de563546f70cef03c0f07b34f3378316c1f601e5
nvnptit <nvnptit@gmail.com>
2022-05-20 11:01:09 +0700

    done

--- GetLocation/ViewController.swift
+++ GetLocation/ViewController.swift
@@ -5,12 +5,31 @@
 //  Created by Nhat on 20/05/2022.
 //
 
+import Foundation
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    @IBOutlet var label : UILabel!
+    var manager: CLLocationManager?
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first  else {
+            return
+        }
+        label.text = "Kinh độ: \(first.coordinate.longitude) \nVĩ độ: \(first.coordinate.latitude) \n "
+    }
 }
+
 
